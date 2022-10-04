2e160e97cb11f2f24c5fccbae8a6ae98cb277909
zoz0312 <zoz0312@naver.com>
2022-10-05 00:59:50 +0900

    [feat] 초기 환경 세팅

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    'babel-plugin-styled-components',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
+  ],
+  presets: [
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    ['@babel/preset-env', { targets: { node: 'current' } }],
+    '@babel/preset-typescript',
+  ],
+};
