8ef2189f1b91e8d1ac0fda330bf3480038b5d5ff
zoz0312 <zoz0312@naver.com>
2022-10-05 03:44:28 +0900

    [fix] infinityQuery 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { getImages } from 'apis/remotes';
 import { GET_ITEM_SIZE } from 'constants/common';
 
 function App() {
-  const { data } = useQuery(['getImages'], () => {
-    return getImages({ query: 'kia', size: GET_ITEM_SIZE, sort: 'accuracy' });
-  });
+  const { data, fetchNextPage } = useInfiniteQuery(
+    ['getImages'],
+    async ({ pageParam = 1 }) => {
+      const { data } = await getImages({ query: 'kia', page: pageParam, size: GET_ITEM_SIZE, sort: 'accuracy' });
+      return {
+        ...data,
+        nextPage: pageParam + 1,
+      };
+    },
+    {
+      getNextPageParam: lastPage => {
+        return lastPage.nextPage;
+      },
+    }
+  );
 
-  console.log('data', data?.data);
+  console.log('data', data?.pages);
   return (
     <div className="App">
       <header className="App-header">
@@ -15,6 +27,7 @@ function App() {
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
+        <button onClick={() => fetchNextPage()}>Fetch Next </button>
       </header>
     </div>
   );
